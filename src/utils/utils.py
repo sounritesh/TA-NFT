bug fix scaling

src/utils/utils.py
@@ -8,7 +8,7 @@ class MinMaxScaler():
         self.x_max = torch.max(x).to(device)
 
     def transform(self, x):
-        return (x - self.x_min)/(self.x_max - self.x_min)
+        return (torch.tensor(x) - self.x_min)/(self.x_max - self.x_min)
 
     def inverse_transform(self, out):
         return torch.tensor(out)*(self.x_max - self.x_min) + self.x_min