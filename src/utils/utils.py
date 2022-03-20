cd6e26ada1feb520719cf6bc95220d0f7c126738
Ritesh Soun <sounritesh@gmail.com>
2022-03-21 01:07:52 +0530

    bug fix

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,9 +6,10 @@ class MinMaxScaler():
         x = torch.tensor(x)
         self.x_min = torch.min(x).to(device)
         self.x_max = torch.max(x).to(device)
+        self.device = device
 
     def transform(self, x):
-        return (torch.tensor(x) - self.x_min)/(self.x_max - self.x_min)
+        return (torch.tensor(x).to(self.device) - self.x_min)/(self.x_max - self.x_min)
 
     def inverse_transform(self, out):
         return torch.tensor(out)*(self.x_max - self.x_min) + self.x_min
