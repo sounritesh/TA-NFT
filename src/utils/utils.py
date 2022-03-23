9e82b9e11a56043d3f445cf96e07a43aa310171f
Ritesh Soun <sounritesh@gmail.com>
2022-03-23 20:03:34 +0530

    scale change

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -9,7 +9,7 @@ class MinMaxScaler():
         self.device = device
 
     def transform(self, x):
-        return ((torch.tensor(x).to(self.device) - self.x_min)*10)/(self.x_max - self.x_min)
+        return ((torch.tensor(x).to(self.device) - self.x_min)*100)/(self.x_max - self.x_min)
 
     def inverse_transform(self, out):
         return out*(self.x_max - self.x_min) + self.x_min
